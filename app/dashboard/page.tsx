ebb0a1033f5df7242fd5bb9636be3aaccb31da2c
Nicolas GONZALEZ <ngonzalez@warmango.fr>
2024-02-06 12:58:54 +0100

    WIP

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,37 +2,28 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { inter } from '@/app/ui/fonts';
-import { gql, useQuery } from '@apollo/client';
+import axios from "axios";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const GET_ACCOUNTS = gql`
-    query Account {
-      accounts {
-          name
-          email
-          interests
-          accountType
-          createdAt
-      }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let apiResponse = [];
+  let results = await axios.post(
+    "http://127.0.0.1:3000/graphql",
+    {
+      // Stuck here on searchType
+      query: `
+        query Account {
+          accounts {
+            name
+            email
+            interests
+            accountType
+            createdAt
+          }
+        }
+      `,
     }
-  `;
+  ).then((result) => {
+    console.debug(result.data);
+  });
 
-  const {error, loading, data} = useQuery(GET_ACCOUNTS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  return <main>
-           <h1 className="mb-4 text-xl md:text-2xl">
-             Accounts
-           </h1>
-          <div>
-            {data.accounts.map((account) => (
-              <div key={user.id}>
-                <h3>{user.name}</h3>
-                <p>{user.email}</p>
-                <p>{user.interests}</p>
-              </div>
-            ))}
-          </div>
-         </main>
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import {NextUIProvider} from '@nextui-org/react'
-import {ApolloProvider} from '@apollo/client';
-import client from './lib/apollo';
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <ApolloProvider client={client}>
-        {children}
-      </ApolloProvider>
+    {children}
     </NextUIProvider>
   )
 }
