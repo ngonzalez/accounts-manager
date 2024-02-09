d679f03e4f7c327ca79d87f2277fd85ac0bfa903
Nicolas GONZALEZ <ngonzalez@warmango.fr>
2024-02-09 11:18:24 +0100

    Fix GraphQL query, remove optional field

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,7 +15,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           accounts {
             name
             email
-            interests
             accountType
             createdAt
           }
