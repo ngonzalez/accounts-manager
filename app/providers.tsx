7f82b8e815e0a84cd8d49493890aad6cf397c6ab
Nicolas GONZALEZ <ngonzalez@warmango.fr>
2024-02-09 14:59:53 +0100

    Remove unused provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,12 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import { Client, cacheExchange, fetchExchange } from '@urql/core'
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import client from "@/app/lib/apollo";
 
 export function Providers({children}: { children: React.ReactNode }) {
 
   return (
-    <ApolloProvider client={client}>
-      <NextUIProvider>
-        {children}
-      </NextUIProvider>
-    </ApolloProvider>
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
   )
 }
